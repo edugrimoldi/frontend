ce9cec39455bcaf1e468f5d85d255b8dd031d45a
maite-ai <mariafernandarios89@gmail.com>
2023-09-14 18:48:12 -0300

    Make lil changes

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import shutil
 import base64
 
 st.set_page_config(
-            page_title="Gameplay Video Auto Edit",
-            #page_icon="",
+            page_title="Gameplay VAE",
+            page_icon="https://www.iconpacks.net/icons/1/free-video-icon-831-thumb.png",
             layout="centered")
 
 CSS = """
@@ -16,62 +16,55 @@ CSS = """
 * {
     font-family: 'Chakra Petch', monospace, helvetica;
     font-weight: bold;
+    color: white;
 }
 
 h1 {
-    color: white;
     text-shadow: 4px 2px 6px #9aa19b;
 }
 
 .stApp {
-    background-color: #021f06;
+    background-color: #1f2120;
 }
+
 """
 
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-#load_dotenv()
 url = 'https://auto-edit-vkmckhunoq-uc.a.run.app'
 
 # App title and description
-st.header('Video Auto Edit')
+st.header('Gameplay Video Auto Edit')
 st.markdown('''
             > This app will return the best clips of your gameplay video
             ''')
 
 st.markdown("---")
 
-# Create a native Streamlit file upload input
-st.markdown("Choose a video from your computer 👇")
-
 st.set_option('deprecation.showfileUploaderEncoding', False)
-
-uploaded_file = st.file_uploader("Upload a .mp4 file", type="mp4")
+uploaded_file = st.file_uploader("Upload a .mp4 file 👇", type="mp4")
 
 if uploaded_file is not None:
-    #st.video(uploaded_file)
-    st.write("Uploaded succesfully")
-
+    
     if st.button('Process the video'):
         with st.spinner("Wait for it..."):
             ### Get bytes from the file buffer
             video_bytes = uploaded_file.read()
 
             files = {'file': video_bytes}
-
+            
             ### Make request to the API
             with requests.post(url + "/predict", files=files, stream=True) as res:
 
                 if res.status_code == 200:
-                    st.markdown("File processed succesfully! ✅")
+                    st.write("File processed succesfully! ✅")
                     response = res.json()
                     test_dict = json.loads(response)
                     df = pd.DataFrame.from_dict(test_dict)
                     csv = df.to_csv(header=False)
-                    
-                    st.markdown("---")
 
-                    st.write("Your csv is ready!")
+                    st.markdown("### Your markers are ready!")
+                    st.write("Save .csv file 👇")
 
                     st.download_button(
                     "Download",
@@ -90,5 +83,5 @@ st.markdown("---")
 st.markdown('''
 
 
-            > * **Visit** our [repo](http://github.com/edugrimoldi/auto-edit) in Github
+            > *Visit our [repo](http://github.com/edugrimoldi/auto-edit) in Github*
             ''')
